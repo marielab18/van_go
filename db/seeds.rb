6fa81ceda690e3bd9574b5425ab4aeda3aaaaa58
Maria Sutanto <maria.w.sutanto@gmail.com>
2022-02-23 15:25:24 +0100

    added photos to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require "faker"
+require "open-uri"
 
 10.times do
   van = Van.new(
@@ -18,6 +19,11 @@ require "faker"
   )
   van.user = User.first
   van.save
+end
+
+Van.all.each do |van|
+  file = URI.open('https://images.unsplash.com/photo-1527786356703-4b100091cd2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80')
+  van.photo.attach(io: file, filename: 'banana.png', content_type: 'image/png')
 end
 
 # 10.times do |i|
