2200ee36199f31f2b9753a9028fa76f031aab9e7
Mariela Belaunde <mariela.belaunde@gmail.com>
2022-02-24 11:29:24 +0100

    Geo

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'faker'
+
+gem "geocoder"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_115300) do
+ActiveRecord::Schema.define(version: 2022_02_24_102740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_115300) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
